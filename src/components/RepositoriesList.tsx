03ae48e8040877a74d2c6a3b81038faeceffae8d
Rory Cawley <rory.cawley@gmail.com>
2021-03-13 14:22:45 +0000

    final commit

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useActions, useTypedSelector } from "../hooks";
+
+const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
+  // console.log(state);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    searchRepositories(term);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <li key={name}>{name}</li>)}
+    </div>
+  );
+};
+
+export default RepositoriesList;
--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,24 +1,32 @@
-import { ActionType } from './../action-types';
-import { Action } from '../actions'
+import { ActionType } from "./../action-types";
+import { Action } from "../actions";
 
 interface RepositoriesState {
-
-    loading: boolean
-    error: string | null
-    data: string[]
+  loading: boolean;
+  error: string | null;
+  data: string[];
 }
 
-const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
-    switch (action.type) {
-        case ActionType.SEARCH_REPOSITORIES:
-            return { loading: true, error: null, data: [] }
-        case ActionType.SEARCH_REPOSITORIES_SUCCESS:
-            return { loading: false, error: null, data: action.payload }
-        case ActionType.SEARCH_REPOSITORIES_ERROR:
-            return { loading: false, error: action.payload, data: [] }
-        default:
-            return state
-    }
-}
+const initialState = {
+  loading: false,
+  error: null,
+  data: [],
+};
 
-export default reducer
+const reducer = (
+  state: RepositoriesState = initialState,
+  action: Action
+): RepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return { loading: false, error: null, data: action.payload };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
